6f97f4588e3f3343fd171894ca6abedfaa808a04
TunaYasa <134148277+TunaYasa@users.noreply.github.com>
2023-10-26 20:40:13 +0300

    Update external_device_checker.ps1

--- external_device_checker.ps1
+++ external_device_checker.ps1
@@ -1,15 +1,14 @@
 # Define an action to trigger when a USB device is connected
-
 Register-WmiEvent -Query "SELECT * FROM __InstanceCreationEvent WITHIN 1 WHERE TargetInstance ISA 'Win32_PnPEntity' AND TargetInstance.Description LIKE 'USB%'" -Action {
-
+    # USB device variable
     $usbDevice = [System.Management.ManagementBaseObject]$Event.SourceEventArgs.NewEvent.TargetInstance
 
+    # Make a condition for if connected external device is USB
     if ($usbDevice.Description -match "USB") {
-
+        # Get output when USB is inserted
         Write-Host "USB device inserted: $($usbDevice.Description)"
 
         # You can add the operations you want to do when USB is connected
-
         Start-Process -FilePath "FILE PATH SHOULD BE ADDED HERE TO RUN FILE OR .EXE"
     }
 }
